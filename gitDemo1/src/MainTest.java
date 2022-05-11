12cc6eaf45cc2080f1ea7ce5ffeb3495cb351b23
lbs20020809 <212376035@qq.com>
2022-05-11 20:15:19 +0800

    第二次提交

--- gitDemo1/src/MainTest.java
+++ gitDemo1/src/MainTest.java
@@ -1,6 +1,7 @@
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("I love you");
-        String a;
+        String a="aaa";
+        System.out.println(a);
     }
 }
